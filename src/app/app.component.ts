547ff768e2b82d318d46fc71a5677da0d771ce4f
Jose-Oquendo <oquendojuanjose@gmail.com>
2025-04-27 19:33:10 -0500

    Creation of personal portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.less'
 })
-export class AppComponent {
-  title = 'oquendo.soft';
+export class AppComponent implements OnInit {
+  @ViewChild('starsContainer', { static: true }) starsContainer!: ElementRef;
+  numStars: number = 100;
+
+  ngOnInit() {
+    const container = this.starsContainer.nativeElement;
+    container.innerHTML = '';
+
+    for (let i = 0; i < this.numStars; i++) {
+      let star = document.createElement('div');
+      star.classList.add('star');
+      star.style.top = Math.random() * 100 + 'vh';
+      star.style.left = Math.random() * 100 + 'vw';
+
+      let isLarge = Math.random() > 0.9;
+      let size = isLarge ? Math.random() * 4 + 2 : 2;
+      star.style.width = size + 'px';
+      star.style.height = size + 'px';
+
+      star.style.background = isLarge ? 'lightblue' : 'white';
+      star.style.animationDuration = (Math.random() * 4 + 6) + 's';
+      star.style.animationDelay = (Math.random() * 3) + 's';
+
+      container.appendChild(star);
+    }
+  }
 }
---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation',
+  imports: [],
+  templateUrl: './navigation.component.html',
+  styleUrl: './navigation.component.less'
+})
+export class NavigationComponent {
+
+}
